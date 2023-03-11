9b9ee0e3bdbf45af56b5f487a5368891c4468146
Hanan574 <hannimuhammad574@gmail.com>
2023-03-11 17:32:54 +0000

    revised the JS file and edited the CSS

--- src/app.js
+++ src/app.js
@@ -1,20 +1,45 @@
-let city = "London";
-let apiKey = "2e4de2b11b8a6f23faaa2d921b3e9a8f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+let p = document.querySelector(".cities");
+p.innerHTML = `${day},  ${hour}:${minute}`;
+
 function weather(response) {
   console.log(response);
+  document.querySelector(".country").innerHTML = response.data.name;
+
   let temp = Math.round(response.data.main.temp);
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${temp}°C`;
+  document.querySelector(".city").innerHTML = `${temp}°C`;
 
-  let wind = document.querySelector(".wind");
-  wind.innerHTML = response.data.weather[0].main;
+  document.querySelector(
+    ".windy"
+  ).innerHTML = `Description : ${response.data.weather[0].main}`;
 
-  let humid = document.querySelector(".humidity");
-  humid.innerHTML = response.data.main.humidity;
-
-  let cities = document.querySelector("h1");
-  cities.innerHTML = `${city}`;
+  document.querySelector(
+    ".humid"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}`;
 }
 
-axios.get(apiUrl).then(weather);
+function lookUp(event) {
+  event.preventDefault();
+  let apiKey = "2e4de2b11b8a6f23faaa2d921b3e9a8f";
+  let city = document.querySelector("#search-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(weather);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", lookUp);
